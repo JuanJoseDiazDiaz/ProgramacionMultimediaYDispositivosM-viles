58b553ca40396b898a7fde89e0f6a4dc6c87942b
elpapass <juanjosdazisla@gmail.com>
2024-09-17 21:08:12 +0200

    -- in process

--- src/Kotlin/main+.kt
+++ src/Kotlin/main+.kt
@@ -1,2 +1,18 @@
 package Kotlin
 
+fun main() {
+    println("hola mundo")
+    println("soy juan jose")
+    println("2 dam")
+
+    //todo primer comentario
+    println(3 + 3)
+
+
+
+    val nombre: String = "Juan"
+    val edad = 12
+
+    println(nombre)
+    println(edad)
+}
